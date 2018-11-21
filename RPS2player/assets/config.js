2216919991c955b7b1ea022a431d5725715429ad
bcihaveto <bcihaveto@gmail.com>
2018-11-21 15:48:50 -0600

    first draft

---
+++ RPS2player/assets/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBZSLwsJCvP5OiX0rFaNMT1S6vSbs5lRAE",
+    authDomain: "prs2-2560e.firebaseapp.com",
+    databaseURL: "https://prs2-2560e.firebaseio.com",
+    projectId: "prs2-2560e",
+    storageBucket: "prs2-2560e.appspot.com",
+    messagingSenderId: "270091133375"
+  };
+  firebase.initializeApp(config);
